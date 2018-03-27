0bdc07e6d5ab63f61d73b85f3fc758472a3cfdf2
Massamba Sene <akimar@live.fr>
2018-03-27 15:48:18 +0200

    creation MusiqueWindow

--- Windows/MenuMusique.Designer.cs
+++ Windows/MenuMusique.Designer.cs
@@ -28,14 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuMusique));
             this.buttonClose = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonAjout = new System.Windows.Forms.Button();
+            this.buttonModifier = new System.Windows.Forms.Button();
+            this.buttonSupprimer = new System.Windows.Forms.Button();
+            this.buttonLire = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonClose
             // 
-            this.buttonClose.Location = new System.Drawing.Point(711, 586);
+            this.buttonClose.Location = new System.Drawing.Point(741, 572);
             this.buttonClose.Name = "buttonClose";
             this.buttonClose.Size = new System.Drawing.Size(75, 23);
             this.buttonClose.TabIndex = 0;
@@ -46,22 +53,73 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(28, 25);
+            this.dataGridView1.Location = new System.Drawing.Point(118, 25);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(788, 538);
+            this.dataGridView1.Size = new System.Drawing.Size(698, 505);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // buttonAjout
+            // 
+            this.buttonAjout.Location = new System.Drawing.Point(12, 76);
+            this.buttonAjout.Name = "buttonAjout";
+            this.buttonAjout.Size = new System.Drawing.Size(75, 23);
+            this.buttonAjout.TabIndex = 2;
+            this.buttonAjout.Text = "Ajouter";
+            this.buttonAjout.UseVisualStyleBackColor = true;
+            // 
+            // buttonModifier
+            // 
+            this.buttonModifier.Location = new System.Drawing.Point(12, 194);
+            this.buttonModifier.Name = "buttonModifier";
+            this.buttonModifier.Size = new System.Drawing.Size(75, 23);
+            this.buttonModifier.TabIndex = 3;
+            this.buttonModifier.Text = "Modifier";
+            this.buttonModifier.UseVisualStyleBackColor = true;
+            // 
+            // buttonSupprimer
+            // 
+            this.buttonSupprimer.Location = new System.Drawing.Point(12, 316);
+            this.buttonSupprimer.Name = "buttonSupprimer";
+            this.buttonSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.buttonSupprimer.TabIndex = 4;
+            this.buttonSupprimer.Text = "Supprimer";
+            this.buttonSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // buttonLire
+            // 
+            this.buttonLire.Location = new System.Drawing.Point(37, 572);
+            this.buttonLire.Name = "buttonLire";
+            this.buttonLire.Size = new System.Drawing.Size(75, 23);
+            this.buttonLire.TabIndex = 5;
+            this.buttonLire.Text = "Lire";
+            this.buttonLire.UseVisualStyleBackColor = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(141, 556);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(564, 46);
+            this.axWindowsMediaPlayer1.TabIndex = 6;
             // 
             // MenuMusique
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(857, 632);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.buttonLire);
+            this.Controls.Add(this.buttonSupprimer);
+            this.Controls.Add(this.buttonModifier);
+            this.Controls.Add(this.buttonAjout);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonClose);
             this.Name = "MenuMusique";
             this.Text = "Menu Musique";
             this.Load += new System.EventHandler(this.MenuMusique_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,5 +128,10 @@
 
         private System.Windows.Forms.Button buttonClose;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonAjout;
+        private System.Windows.Forms.Button buttonModifier;
+        private System.Windows.Forms.Button buttonSupprimer;
+        private System.Windows.Forms.Button buttonLire;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
---
+++ Windows/MusiqueWindow.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace LecteurMusique.Windows
+{
+    partial class MusiqueWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAnnuler = new System.Windows.Forms.Button();
+            this.buttonValider = new System.Windows.Forms.Button();
+            this.dateTimePickerNaissance = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxPays = new System.Windows.Forms.ComboBox();
+            this.textBoxVille = new System.Windows.Forms.TextBox();
+            this.textBoxCP = new System.Windows.Forms.TextBox();
+            this.textBoxAdresse = new System.Windows.Forms.TextBox();
+            this.textBoxPrenom = new System.Windows.Forms.TextBox();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.labelPays = new System.Windows.Forms.Label();
+            this.labelNaissance = new System.Windows.Forms.Label();
+            this.labelVille = new System.Windows.Forms.Label();
+            this.labeCP = new System.Windows.Forms.Label();
+            this.labelAdresse = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.labelTitre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonAnnuler
+            // 
+            this.buttonAnnuler.Location = new System.Drawing.Point(290, 306);
+            this.buttonAnnuler.Name = "buttonAnnuler";
+            this.buttonAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnnuler.TabIndex = 31;
+            this.buttonAnnuler.Text = "Annuler";
+            this.buttonAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // buttonValider
+            // 
+            this.buttonValider.Location = new System.Drawing.Point(103, 306);
+            this.buttonValider.Name = "buttonValider";
+            this.buttonValider.Size = new System.Drawing.Size(75, 23);
+            this.buttonValider.TabIndex = 30;
+            this.buttonValider.Text = "Valider";
+            this.buttonValider.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePickerNaissance
+            // 
+            this.dateTimePickerNaissance.Location = new System.Drawing.Point(170, 212);
+            this.dateTimePickerNaissance.Name = "dateTimePickerNaissance";
+            this.dateTimePickerNaissance.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerNaissance.TabIndex = 29;
+            // 
+            // comboBoxPays
+            // 
+            this.comboBoxPays.FormattingEnabled = true;
+            this.comboBoxPays.Location = new System.Drawing.Point(170, 241);
+            this.comboBoxPays.Name = "comboBoxPays";
+            this.comboBoxPays.Size = new System.Drawing.Size(144, 21);
+            this.comboBoxPays.TabIndex = 28;
+            // 
+            // textBoxVille
+            // 
+            this.textBoxVille.Location = new System.Drawing.Point(170, 177);
+            this.textBoxVille.Name = "textBoxVille";
+            this.textBoxVille.Size = new System.Drawing.Size(144, 20);
+            this.textBoxVille.TabIndex = 27;
+            // 
+            // textBoxCP
+            // 
+            this.textBoxCP.Location = new System.Drawing.Point(170, 142);
+            this.textBoxCP.Name = "textBoxCP";
+            this.textBoxCP.Size = new System.Drawing.Size(144, 20);
+            this.textBoxCP.TabIndex = 26;
+            // 
+            // textBoxAdresse
+            // 
+            this.textBoxAdresse.Location = new System.Drawing.Point(170, 104);
+            this.textBoxAdresse.Name = "textBoxAdresse";
+            this.textBoxAdresse.Size = new System.Drawing.Size(144, 20);
+            this.textBoxAdresse.TabIndex = 25;
+            // 
+            // textBoxPrenom
+            // 
+            this.textBoxPrenom.Location = new System.Drawing.Point(170, 73);
+            this.textBoxPrenom.Name = "textBoxPrenom";
+            this.textBoxPrenom.Size = new System.Drawing.Size(144, 20);
+            this.textBoxPrenom.TabIndex = 24;
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(170, 37);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(144, 20);
+            this.textBoxNom.TabIndex = 23;
+            // 
+            // labelPays
+            // 
+            this.labelPays.AutoSize = true;
+            this.labelPays.Location = new System.Drawing.Point(71, 244);
+            this.labelPays.Name = "labelPays";
+            this.labelPays.Size = new System.Drawing.Size(30, 13);
+            this.labelPays.TabIndex = 22;
+            this.labelPays.Text = "Pays";
+            // 
+            // labelNaissance
+            // 
+            this.labelNaissance.AutoSize = true;
+            this.labelNaissance.Location = new System.Drawing.Point(68, 212);
+            this.labelNaissance.Name = "labelNaissance";
+            this.labelNaissance.Size = new System.Drawing.Size(96, 13);
+            this.labelNaissance.TabIndex = 21;
+            this.labelNaissance.Text = "Date de naissance";
+            // 
+            // labelVille
+            // 
+            this.labelVille.AutoSize = true;
+            this.labelVille.Location = new System.Drawing.Point(71, 177);
+            this.labelVille.Name = "labelVille";
+            this.labelVille.Size = new System.Drawing.Size(26, 13);
+            this.labelVille.TabIndex = 20;
+            this.labelVille.Text = "Ville";
+            // 
+            // labeCP
+            // 
+            this.labeCP.AutoSize = true;
+            this.labeCP.Location = new System.Drawing.Point(68, 142);
+            this.labeCP.Name = "labeCP";
+            this.labeCP.Size = new System.Drawing.Size(64, 13);
+            this.labeCP.TabIndex = 19;
+            this.labeCP.Text = "Code Postal";
+            // 
+            // labelAdresse
+            // 
+            this.labelAdresse.AutoSize = true;
+            this.labelAdresse.Location = new System.Drawing.Point(65, 104);
+            this.labelAdresse.Name = "labelAdresse";
+            this.labelAdresse.Size = new System.Drawing.Size(45, 13);
+            this.labelAdresse.TabIndex = 18;
+            this.labelAdresse.Text = "Adresse";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(65, 73);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(43, 13);
+            this.label.TabIndex = 17;
+            this.label.Text = "Prénom";
+            // 
+            // labelTitre
+            // 
+            this.labelTitre.AutoSize = true;
+            this.labelTitre.Location = new System.Drawing.Point(65, 37);
+            this.labelTitre.Name = "labelTitre";
+            this.labelTitre.Size = new System.Drawing.Size(28, 13);
+            this.labelTitre.TabIndex = 16;
+            this.labelTitre.Text = "Titre";
+            // 
+            // MusiqueWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 474);
+            this.Controls.Add(this.buttonAnnuler);
+            this.Controls.Add(this.buttonValider);
+            this.Controls.Add(this.dateTimePickerNaissance);
+            this.Controls.Add(this.comboBoxPays);
+            this.Controls.Add(this.textBoxVille);
+            this.Controls.Add(this.textBoxCP);
+            this.Controls.Add(this.textBoxAdresse);
+            this.Controls.Add(this.textBoxPrenom);
+            this.Controls.Add(this.textBoxNom);
+            this.Controls.Add(this.labelPays);
+            this.Controls.Add(this.labelNaissance);
+            this.Controls.Add(this.labelVille);
+            this.Controls.Add(this.labeCP);
+            this.Controls.Add(this.labelAdresse);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.labelTitre);
+            this.Name = "MusiqueWindow";
+            this.Text = "MusiqueWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAnnuler;
+        private System.Windows.Forms.Button buttonValider;
+        private System.Windows.Forms.DateTimePicker dateTimePickerNaissance;
+        private System.Windows.Forms.ComboBox comboBoxPays;
+        private System.Windows.Forms.TextBox textBoxVille;
+        private System.Windows.Forms.TextBox textBoxCP;
+        private System.Windows.Forms.TextBox textBoxAdresse;
+        private System.Windows.Forms.TextBox textBoxPrenom;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.Label labelPays;
+        private System.Windows.Forms.Label labelNaissance;
+        private System.Windows.Forms.Label labelVille;
+        private System.Windows.Forms.Label labeCP;
+        private System.Windows.Forms.Label labelAdresse;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label labelTitre;
+    }
+}
