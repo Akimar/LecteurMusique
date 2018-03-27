59b8ac507254c80474d4a690144943f057597326
Massamba Sene <akimar@live.fr>
2018-03-27 14:47:01 +0200

    Ajout musique et album repository

---
+++ BDD/AlbumRepository.cs
@@ -0,0 +1,38 @@
+﻿using LecteurMusique.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LecteurMusique.BDD
+{
+    public static class AlbumRepository
+    {
+
+        public static string getAlbumLibelle(long identifiantAlbum)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection("Server=localhost;Database=BaseDeDonneesLecteur;Trusted_Connection=True;");
+
+                SqlCommand commande = new SqlCommand();
+                commande.CommandText = @"SELECT Nom FROM Album WHERE Identifiant = @identifiantAlbum";
+                commande.Connection = connection;
+
+                connection.Open();
+
+                commande.Prepare();
+                commande.Parameters.AddWithValue("@identifiantAlbum", identifiantAlbum);
+
+                return commande.ExecuteScalar().ToString();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+    }
+}
