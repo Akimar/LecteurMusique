83d34090c51501151ce6ab7c2a879af07af39284
Massamba Sene <akimar@live.fr>
2018-03-29 21:19:08 +0200

    Ajout classe Artiste

---
+++ Classes/Artiste.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LecteurMusique.Classes
+{
+    public class Artiste
+    {
+        #region Proprietés
+
+        public long Identifiant;
+
+        private string nom;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        #endregion
+
+        #region Constructeurs
+        public Artiste()
+        {
+
+        }
+        #endregion
+    }
+}
